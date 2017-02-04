9316fc54da5ccc889fd7e5abf1d563c6faab8c7d
Patrick Calabrese <PatrickCalabrese@Patricks-MacBook-Pro.local>
2017-02-03 22:03:37 -0500

    Inserted function for when convert button is pressed. Will show error if user input is not a string that can be read as a double

--- $125 Million App/ViewController.swift
+++ $125 Million App/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        
+        if let miles = Double(userInput.text!) {
+            let km = miles * 1.6
+            resultsLabel.text = "\(miles) miles = \(km) km"
+        } else {
+            resultsLabel.text = "\(userInput.text!) is not a valid number!"
+        }
+    }
 
 }
 
